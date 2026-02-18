795ecf1b9486ea14fe7db03fa9185c26663ec498
Mahmoud Alazzeh <mma1365@g.rit.edu>
2026-02-18 14:44:36 +0400

    Add Customer and Order beans

---
+++ src/main/java/org/example/esouq/model/Order.java
@@ -0,0 +1,35 @@
+package org.example.esouq.model;
+
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Order {
+    private int orderId;
+    private String orderStatus;
+    private String shippingAddress;
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setOrderStatus(String orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+
+    public String getShippingAddress() {
+        return shippingAddress;
+    }
+
+    public void setShippingAddress(String shippingAddress) {
+        this.shippingAddress = shippingAddress;
+    }
+}
