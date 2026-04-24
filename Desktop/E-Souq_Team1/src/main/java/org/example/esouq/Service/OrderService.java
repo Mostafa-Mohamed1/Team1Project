b72a8bcd6416f703c7412038bbf44f509741e8d4
Mahmoud Alazzeh <mma1365@g.rit.edu>
2026-04-24 20:59:07 +0300

    Added implementation of Order service and Repository and added all related methods

--- Desktop/E-Souq_Team1/src/main/java/org/example/esouq/Service/OrderService.java
+++ Desktop/E-Souq_Team1/src/main/java/org/example/esouq/Service/OrderService.java
@@ -1,4 +1,38 @@
 package org.example.esouq.Service;
 
+import jakarta.transaction.Transactional;
+import org.example.esouq.model.Order;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.example.esouq.repositories.OrderDAO;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class OrderService {
+
+    @Autowired
+    private OrderDAO orderDao;
+
+    public Order saveOrder(Order order){
+        return orderDao.save(order);
+    }
+    public List<Order> getAllOrders() {
+        return orderDao.findAll();
+    }
+    public Optional<Order> getOrderById(int id) {
+        return orderDao.findById(id);
+    }
+    public List<Order> getOrdersByStatus(String status) {
+        return orderDao.findByOrderStatus(status);
+    }
+    @Transactional
+    public void updateStatus(int id, String status) {
+        orderDao.updateOrderStatusById(id, status);
+    }
+
+    public void deleteOrder(int id) {
+        orderDao.deleteById(id);
+    }
 }
