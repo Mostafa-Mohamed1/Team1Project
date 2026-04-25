902343790b467d15c4d4e47bda64d2817dd02ac1
Bechir Zaied <bb3462@rit.edu>
2026-04-26 00:15:33 +0400

    added category bean in jpa annotation (long overdue we needed 4 from the start)

---
+++ Desktop/E-Souq_Team1/src/main/java/org/example/esouq/model/Category.java
@@ -0,0 +1,34 @@
+package org.example.esouq.model;
+import jakarta.persistence.*;
+import java.util.List;
+
+
+@Entity
+@Table(name= "categories")
+
+public class Category {
+
+    public Category(){} //used to build obj from db
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int categoryId;
+
+    @Column(length = 100)
+    private String categoryName;
+
+    @Column(length = 150)
+    private String categoryDescription;
+
+    @OneToMany(mappedBy = "category")
+    private List<Product> products;
+
+    public int getCategoryId() {return categoryId;}
+    public String getCategoryName() {return categoryName;}
+    public String getCategoryDescription() {return categoryDescription;}
+    public List<Product> getProducts() {return products;}
+
+    public void setCategoryName(String categoryName) {this.categoryName = categoryName;}
+    public void setCategoryDescription(String categoryDescription) {this.categoryDescription = categoryDescription;}
+    public void setProducts(List<Product> products) {this.products = products;}
+}
