d12402e0bd5d859f9c50e27bd5694154fd2071e2
Bechir Zaied <bb3462@rit.edu>
2026-04-26 18:44:47 +0400

    added customer controller with 6 restapi methods

---
+++ Desktop/E-Souq_Team1/src/main/java/org/example/esouq/controllers/CustomerController.java
@@ -0,0 +1,67 @@
+package org.example.esouq.controllers;
+
+import org.example.esouq.model.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.util.Optional;
+
+
+@RestController
+@RequestMapping("/api/customers")
+public class CustomerController {
+
+    @Autowired
+    private CustomerService customerService;
+
+    // get all customers
+    @GetMapping
+    public List<Customer> getAllCustomers() {
+        return customerService.getAllCustomers();
+    }
+
+    // get customer by its id
+    @GetMapping("/{id}")
+    public ResponseEntity<Customer> getCustomerById(@PathVariable int id) {
+        Optional<Customer> customer = customerService.getCustomerById(id);
+
+        if (customer.isPresent()) {
+            return ResponseEntity.ok(customer.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    // get customer by email (search)
+    @GetMapping("/search")
+    public List<Customer> getCustomersByEmail(@RequestParam String email) {
+        return customerService.getCustomersByEmail(email);
+    }
+
+    // create new customer
+    @PostMapping
+    public ResponseEntity<Customer> createCustomer(@RequestBody Customer customer) {
+        Customer savedCustomer = customerService.saveCustomer(customer);
+        return new ResponseEntity<>(savedCustomer, HttpStatus.CREATED);
+    }
+
+    // update existing customer
+    @PutMapping("/{id}")
+    public ResponseEntity<Customer> updateCustomer(@PathVariable int id, @RequestBody Customer customer) {
+        Customer updatedCustomer = customerService.updateById(id, customer);
+        if (updatedCustomer != null) {
+            return ResponseEntity.ok(updatedCustomer);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    // delete a custoemr
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCustomer(@PathVariable int id) {
+        customerService.deleteCustomerById(id);
+        return ResponseEntity.noContent().build();
+    }
+}
