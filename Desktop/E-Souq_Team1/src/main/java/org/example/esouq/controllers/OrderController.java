8be0feb1f7e26589a1f53ca85f907cbf31f84da5
Mahmoud Alazzeh <mma1365@g.rit.edu>
2026-04-26 13:27:34 +0300

    Implemented OrderController class

--- Desktop/E-Souq_Team1/src/main/java/org/example/esouq/controllers/OrderController.java
+++ Desktop/E-Souq_Team1/src/main/java/org/example/esouq/controllers/OrderController.java
@@ -1,4 +1,62 @@
 package org.example.esouq.controllers;
 
-public class OrderController {
-}
+import jakarta.transaction.Transactional;
+import org.example.esouq.Service.OrderService;
+import org.example.esouq.model.Order;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+    @RestController
+    @RequestMapping("/api/order")
+    @CrossOrigin(origins = "*")
+    public class OrderController {
+
+        @Autowired
+        private OrderService orderService;
+
+        // GET all orders
+        @GetMapping
+        public List<Order> getAllOrders(){
+            return orderService.getAllOrders();
+        }
+
+        // GET order by id
+        @GetMapping("/{id}")
+        public ResponseEntity<Order> getOrderById(@PathVariable int id){
+            return orderService.getOrderById(id)
+                    .map(ResponseEntity::ok)
+                    .orElse(ResponseEntity.notFound().build());
+        }
+
+        // search order by the status
+        @GetMapping("/search")
+        public List<Order> searchOrderByStatus(String name){
+            return orderService.getOrdersByStatus(name);
+        }
+
+        // create a new order
+        @PostMapping
+        public Order addOrder(@RequestBody Order order){
+            orderService.saveOrder(order);
+            return order;
+        }
+
+        //updates existing Order
+        @PutMapping("/{id}")
+        public ResponseEntity<Order> updateOrder(@PathVariable int id, @RequestBody Order order){
+            order.setOrderId(id);
+            return ResponseEntity.ok(orderService.saveOrder(order));
+        }
+
+        //deletes a Order
+        @DeleteMapping("/{id}")
+        @Transactional
+        public ResponseEntity<Void> deleteOrder(@PathVariable int id) {
+            orderService.deleteOrder(id);
+            return ResponseEntity.noContent().build();
+        }
+    }
+
