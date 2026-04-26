1b66f488cfd6bbfe36b559c8a05296946c6f1f4e
Mostafa Mohamed <mtm1167@g.rit.edu>
2026-04-26 17:53:24 +0400

    added product controller, service, repo

--- Desktop/E-Souq_Team1/src/main/java/org/example/esouq/model/Product.java
+++ Desktop/E-Souq_Team1/src/main/java/org/example/esouq/model/Product.java
@@ -2,49 +2,53 @@ package org.example.esouq.model;
 
 import jakarta.persistence.*;
 
-@Entity()
+@Entity
 @Table(name = "products")
 public class Product {
 
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        @Column(name = "Product_ID")
-        private Long productId;
 
-        @Column(name = "Name", length = 30, nullable = false)
-        private String name;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "Product_ID")
+    private Long productId;
 
-        @Column(name = "Price", nullable = false)
-        private double price;
+    @Column(name = "Name", length = 30, nullable = false)
+    private String name;
 
-    public Product() {};
+    @Column(name = "Price", nullable = false)
+    private double price;
 
-    public Product(String name, double price) {
+    @ManyToOne
+    @JoinColumn(name = "category_id")   // FK for products table
+    private Category category;
+
+    public Product(String name, double price, Category category) {
         this.name = name;
         this.price = price;
+        this.category = category;
     }
+
+    public Product() {}
 
     public Long getProductId() {
         return productId;
     }
-
     public void setProductId(Long productId) {
         this.productId = productId;
     }
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public double getPrice() {
         return price;
     }
-
     public void setPrice(double price) {
         this.price = price;
     }
+
+    public Category getCategory() {return category;}
+    public void setCategory(Category category) {this.category = category;}
 }
